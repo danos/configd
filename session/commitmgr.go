bcfbe1ceda20a68071ebdf0a1a24c484f28222eb
William Ivory <william.ivory@intl.att.com>
2021-04-12 08:13:35 +0100

    Update copyrights
    Part of VRVDR-50443.

--- session/commitmgr.go
+++ session/commitmgr.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2020, AT&T Intellectual Property. All rights reserved.
+// Copyright (c) 2018-2021, AT&T Intellectual Property. All rights reserved.
 //
 // Copyright (c) 2014-2017 by Brocade Communications Systems, Inc.
 // All rights reserved.
--- session/submodule_config_test.go
+++ session/submodule_config_test.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2017,2019, AT&T Intellectual Property. All rights reserved.
+// Copyright (c) 2017,2019, 2021, AT&T Intellectual Property.
+// All rights reserved.
 //
 // SPDX-License-Identifier: LGPL-2.1-only
 //
